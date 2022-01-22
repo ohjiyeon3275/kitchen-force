80826435c4d586b190d4f56ee7acea413378944b
Luppy <55138532+seongkyu-lim@users.noreply.github.com>
2022-01-23 06:43:23 +0900

    refactor: Crud -> Jpa

--- src/main/kotlin/com/kitchenforce/domain/orders/OrderTableRepository.kt
+++ src/main/kotlin/com/kitchenforce/domain/orders/OrderTableRepository.kt
@@ -1,6 +1,6 @@
 package com.kitchenforce.domain.orders
 
-import org.springframework.data.repository.JpaRepository
+import org.springframework.data.jpa.repository.JpaRepository
 
 interface OrderTableRepository: JpaRepository<OrderTable, Long> {
 
