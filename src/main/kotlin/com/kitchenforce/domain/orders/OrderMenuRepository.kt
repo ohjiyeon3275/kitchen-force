1c2f33bf72faf69ef5abb6220c0255df6bac4834
Luppy <55138532+seongkyu-lim@users.noreply.github.com>
2022-01-23 06:44:05 +0900

    refactor: crudrepository -> jparepository

--- src/main/kotlin/com/kitchenforce/domain/orders/OrderMenuRepository.kt
+++ src/main/kotlin/com/kitchenforce/domain/orders/OrderMenuRepository.kt
@@ -1,6 +1,6 @@
 package com.kitchenforce.domain.orders
 
-import org.springframework.data.repository.CrudRepository
+import org.springframework.data.jpa.repository.JpaRepository
 
-interface OrderMenuRepository: CrudRepository<OrderMenu, Long> {
+interface OrderMenuRepository: JpaRepository<OrderMenu, Long> {
 }
