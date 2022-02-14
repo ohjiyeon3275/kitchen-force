6edbfe3410d077c7767b5d5dfa0fcabcfac10531
seongkyu-lim <96x60812@naver.com>
2022-02-14 15:26:56 +0900

    rebase

--- src/main/kotlin/com/kitchenforce/domain/menus/MenuRepository.kt
+++ src/main/kotlin/com/kitchenforce/domain/menus/MenuRepository.kt
@@ -2,7 +2,13 @@ package com.kitchenforce.domain.menus
 
 import org.springframework.data.jpa.repository.JpaRepository
 
+<<<<<<< HEAD
 interface MenuRepository : JpaRepository<Menu, Int> {
 
     fun findByName(name: String): Menu
+=======
+interface MenuRepository: JpaRepository<Menu, Int> {
+
+    fun findByName(name : String): Menu
+>>>>>>> cf9233b (.)
 }
