486b10a5660e584f6e1ad6b9f13152d7aa9b3f47
joungeun <lje0818ovo@naver.com>
2022-02-15 21:50:34 +0900

    [ADD #38] CreateMenu method to MenuService and set isHidden default false

--- src/main/kotlin/com/kitchenforce/service/MenuService.kt
+++ src/main/kotlin/com/kitchenforce/service/MenuService.kt
@@ -2,14 +2,24 @@ package com.kitchenforce.service
 
 import com.kitchenforce.common.exception.NotFoundException
 import com.kitchenforce.domain.menus.Menu
+import com.kitchenforce.domain.menus.MenuGroup
+import com.kitchenforce.domain.menus.MenuGroupRepository
 import com.kitchenforce.domain.menus.MenuRepository
+import com.kitchenforce.dto.menus.MenuCreateRequestDto
 import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
 
 @Service
 class MenuService(
-    private val menuRepository: MenuRepository
+    private val menuRepository: MenuRepository,
+    private val menuGroupRepository: MenuGroupRepository
 ) {
+
+    fun createMenu(req: MenuCreateRequestDto, menuGroup: Int){
+        val group: MenuGroup = menuGroupRepository.findById(menuGroup).orElseThrow();
+        menuRepository.save(Menu(null, req.name, req.price, menuGroup = group));
+    }
+
     fun findAll(): List<Menu> {
         return menuRepository.findAll()
     }
