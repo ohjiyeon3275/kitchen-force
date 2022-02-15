57fb3d05c434627ab8af277a3b2ba5e1cd8ac8f3
joungeun <lje0818ovo@naver.com>
2022-02-15 16:34:23 +0900

    [ADD #38] menuCreate method to MenuController

--- src/main/kotlin/com/kitchenforce/controller/MenuController.kt
+++ src/main/kotlin/com/kitchenforce/controller/MenuController.kt
@@ -1,12 +1,11 @@
 package com.kitchenforce.controller
 
 import com.kitchenforce.domain.menus.Menu
+import com.kitchenforce.dto.menus.MenuCreateRequestDto
 import com.kitchenforce.service.MenuService
 import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.tags.Tag
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @Tag(name = "Menu")
 @RequestMapping("/api/menus")
@@ -14,6 +13,13 @@ import org.springframework.web.bind.annotation.RestController
 class MenuController(
     private val menuService: MenuService,
 ) {
+
+    @Operation(summary = "하나 이상의 상품으로 메뉴를 생성합니다.")
+    @PostMapping("/{menuGroup}")
+    fun menuCreate(req: MenuCreateRequestDto, @PathVariable menuGroup: Int){
+
+    }
+
     @Operation(summary = "메뉴 목록을 반환합니다.")
     @GetMapping("/")
     fun index(): List<Menu> {
