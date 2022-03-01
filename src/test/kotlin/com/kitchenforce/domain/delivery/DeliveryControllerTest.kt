50729b3a104dbbe33555c3b757d7531ee305311d
ohjiyeon3275 <ojy940716@gmail.com>
2022-03-01 15:15:54 +0900

    test-exception코드 추가

--- src/test/kotlin/com/kitchenforce/domain/delivery/DeliveryControllerTest.kt
+++ src/test/kotlin/com/kitchenforce/domain/delivery/DeliveryControllerTest.kt
@@ -1,6 +1,8 @@
 package com.kitchenforce.domain.delivery
 
 import com.fasterxml.jackson.databind.ObjectMapper
+import com.kitchenforce.domain.delivery.exception.DeliveryErrorCodeType
+import com.kitchenforce.domain.delivery.exception.DeliveryException
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
@@ -20,7 +22,7 @@ internal class DeliveryControllerTest @Autowired constructor(
 ){
 
     @Test
-    @DisplayName("배달완료한 주문의 상태를 바꾼다.")
+    @DisplayName("배달완료한 주문의 상태를 바꾼다. -- 200")
     fun `배달완료한 주문의 상태를 바꾼다` () {
 
         //given
@@ -40,7 +42,7 @@ internal class DeliveryControllerTest @Autowired constructor(
         val riderId = 1L
         val deliveryId = 1L
 
-        //when 1) happy result
+        //when
         val performPut = mockMvc.put("/api/delivery/$riderId/$deliveryId"){
             contentType = MediaType.APPLICATION_JSON
             content = objectMapper.writeValueAsString(newRider)
@@ -52,6 +54,41 @@ internal class DeliveryControllerTest @Autowired constructor(
             .andExpect {
                 status { isOk() }
                 jsonPath("$.status") { value("배달완료") }
+            }
+    }
+
+    @Test
+    @DisplayName("배달완료한 주문의 상태를 바꾼다. -- Exception")
+    fun `배달완료한 주문의 상태를 바꾼다-exception` () {
+
+        //given
+        val newDelivery =
+            DeliveryAddress(1L,
+                "주소시 주소동",
+                "010123123",
+                "주문완료",
+                "조심히")
+
+        val newRider =
+            Rider(1L, "rider", "1231231234", listOf(newDelivery))
+
+        deliveryAddressRepository.save(newDelivery)
+        riderRepository.save(newRider)
+
+        val riderId = 1L
+        val deliveryId = 2L // 이부분 오류뜨게 수정
+
+        //when
+        val performPut = mockMvc.put("/api/delivery/$riderId/$deliveryId"){
+            contentType = MediaType.APPLICATION_JSON
+            content = objectMapper.writeValueAsString(newRider)
+        }
+
+        //then
+        performPut
+            .andDo{ print() }
+            .andExpect {
+                status { DeliveryException(DeliveryErrorCodeType.NOT_FOUND_DELIVERY) }
             }
     }
 }
